71f95c423ff2ab9bd30cdb7f2d07b98cba6b850e
CddNotFound <kkcdd@outlook.com>
2025-09-19 13:32:30 +0800

    lab01

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,23 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL) {
+        return 0;
+    }
+
+    node *first = head, *last = head;
+
+    while (last -> next != NULL) {
+        last = last -> next;
+        if (last -> next == NULL) {
+            return 0;
+        }
+        last = last -> next;
+        first = first -> next;
+        if (last == first) {
+            return 1;
+        }
+    }
+
     return 0;
 }
